1947c949545144fbbea872c71622e037e0995015
pikaka <senuang.cai9h@gmail.com>
2021-07-15 19:48:05 -0400

    Second Partt

---
+++ fixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  fixster
+//
+//  Created by Pika Cai on 7/14/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
